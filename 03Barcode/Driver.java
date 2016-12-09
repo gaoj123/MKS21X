44b8028654c63064db6341e287f30267a539dcfe
Jenny Gao <jgao658@gmail.com>
2016-12-08 22:01:56 -0500

    edit Driver

--- 03Barcode/Driver.java
+++ 03Barcode/Driver.java
@@ -2,12 +2,16 @@ public class Driver{
     public static void main (String[] args){
     	Barcode bar=new Barcode("08451");
 	System.out.println(bar);
-	Barcode a=new Barcode("12345");
-	System.out.println(a);
-	Barcode c=a.clone();
-	System.out.println(c);
-	//Barcode b=new Barcode("1.1111");
-	//System.out.println(b);
-	System.out.println(a.compareTo(c));
+	// Barcode a=new Barcode("12345");
+	// System.out.println(a);
+	// Barcode c=a.clone();
+	// System.out.println(c);
+	// //Barcode b=new Barcode("1.1111");
+	// //System.out.println(b);
+	// System.out.println(a.compareTo(c));
+	//System.out.println(Barcode.toZip("||||::|::|::|::|:|:|::::|||::::|"));
+	// System.out.println(Barcode.toCode("12345"));
+      	// System.out.println(new Barcode("12345").compareTo(new Barcode("12434")));
+	// System.out.println(Barcode.toZip("|:::||::|:|::||::|::|:|:|::|:|:|"));
     }
 }
