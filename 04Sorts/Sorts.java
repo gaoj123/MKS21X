1435157dee6dc144eb31701d7e8b40656d879a96
Jenny Gao <jgao658@gmail.com>
2016-12-13 06:36:37 -0500

    create new file

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,27 @@
+public class Sorts{
+  /**EDIT THIS METHOD TO MATCH YOUR NAME 
+  */
+  public static String name(){
+    return "10.Gao.Jenny"; 
+  }
+
+  /**Selection sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void selectionSort(int[] data){
+      for(int s=0;s<data.length;s++){
+	  int smallerInd=0;
+	  for(int i=1;i<data.length;i++){
+	      // if(data[s]>data[i]){
+	      //smallerInd=i;
+	      // }
+	  }
+	  int smallestVal=data[smallerInd];
+	  int frontVal=data[s];
+	  ary[s]=smallestVal;
+	  ary[smaller]=frontVal;	  
+      }
+  }
+    
+}
