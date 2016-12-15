c9ab0c59c12173a4d309c379f726f7c209a6767a
Jenny Gao <jgao658@gmail.com>
2016-12-14 22:00:49 -0500

    make insertionSort functional

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -25,22 +25,67 @@ public class Sorts{
 	}
 	//	return Arrays.toString(data);
     }
-    public static String insertionSort(int[] data){
-	for(int i=0;i<data.length;i++){
-	    for(int j=i+1;j<data.length;j++){
-		if(data[j]<data[i]){
-		    int a=data[j];
-		    data[j]=data[i];
-		    data[i]=a;
+    // public static String insertionSort(int[] data){
+    // 	for(int i=0;i<data.length;i++){
+    // 	    for(int j=i+1;j<data.length;j++){
+    // 		if(data[j]<data[i]){
+    // 		    int a=data[j];
+    // 		    data[j]=data[i];
+    // 		    data[i]=a;
+    // 		}
+    // 	    }
+    // 	}
+    // 	return Arrays.toString(data);
+    // }
+    public static void insertionSort(int[] data){
+	for(int i=1;i<data.length;i++){
+	    int j=i-1;
+	    int a=i;
+	    int valMoving=data[i];
+	    //System.out.println(a);
+	    // System.out.println(valMoving);
+	    while(j>-1){
+		if(data[i]<data[j]){
+		    a=j;
+		    j--;
+		}
+		else {
+		    j=-2;
 		}
 	    }
+	    // for(int j=i-1;j>-1;j--){
+	    // 	int a;
+	    // 	int valMoving;
+	    // 	if (data[i]<data[j]){
+	    // 	    a=j;
+	    // 	    valMoving=data[j];
+	    // 	}
+	    // }
+	    for(int b=i;b>a;b--){
+		data[b]=data[b-1];
+	    }
+	    data[a]=valMoving;
 	}
-	return Arrays.toString(data);
+	// System.out.print("[");
+	// for(int i=0;i<data.length;i++){
+	//     System.out.print(data[i]+",");
+	// }
+	// System.out.println("]");
+	//	return Arrays.toString(data);
+		
     }
     public static void main(String[] args){
-	int[] a=new int[]{64,25,11,22,12};
-	System.out.println(insertionSort(a));
-	// int[] b=new int[]{101,2,67,150,10,1};
-	// System.out.println(selectionSort(b));
+	// int[] a=new int[]{64,25,11,22,12,234,23,33,55,22,33,56};
+	// insertionSort(a);
+	// //	System.out.println(insertionSort(a));
+	// int[] b=new int[]{101,2,67,150,10};
+	// insertionSort(b);
+	// int[] c=new int[]{3, 7, 4, 9, 5, 2, 6, 1};
+	// insertionSort(c);
+	// int[] d=new int[]{9, 10, 4, 3, 0 , 2};
+	// insertionSort(d);
+	// int[] e=new int[]{1,2,4,6, 3,10,0,5};
+	// insertionSort(e);
+	//System.out.println(insertionSort(b));
     }
 }
