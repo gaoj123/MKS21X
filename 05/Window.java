7e922f4bcbc11bb4d3d411d5d44cf17a321827b6
Jenny Gao <jgao658@gmail.com>
2016-12-20 23:35:28 -0500

    create new java file for a window

---
+++ 05/Window.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+public class Window extends JFrame {
+ private Container pane;
+
+ private JButton b;
+ private JLabel l;
+ private JTextField t;
+ private JCheckBox c;
+ 
+  //CONSTRUCTOR SETS EVERYTHING UP
+  public Window() {
+     this.setTitle("My first GUI");
+     this.setSize(600,400);
+     this.setLocation(100,100);
+     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+     pane = this.getContentPane();
+     pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
+     a=new JLabel("Result");
+     b = new JButton("toC");
+     c=new JTextField(12);
+     d=new JButton("toF");
+     //l = new JLabel("This is AWESOME! (lies)",null,JLabel.CENTER);
+     //t = new JTextField(12);
+     // c = new JCheckBox("Overclock!!!");
+     //pane.add(l);
+     pane.add(c);
+     // pane.add(t);
+     pane.add(d);
+     pane.add(b);
+     pane.add(a);
+ }
+
+  //MAIN JUST INSTANTIATES + MAKE VISIBLE
+  public static void main(String[] args) {
+     Window g = new Window();
+     g.setVisible(true);
+  }
+}
